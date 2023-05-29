cbbec190f5ee7de7cbb05f3795b170b65e43f1d9
democrito88 <d_who@hotmail.com>
2023-05-29 15:16:32 -0300

    29-05-2023

--- src/Components/Linhagem.js
+++ src/Components/Linhagem.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { useFetch } from "../hooks/useFetch";
-import { Card } from "react-bootstrap";
+import { Badge, Card } from "react-bootstrap";
 
 
 function Linhagem(props) {
     let parametro = new Object(props);
     let urlLinhagem = new Object(parametro.pokemon.species);
-
-console.log(urlLinhagem.url);
+    let especie = useFetch(urlLinhagem.url);
+    let evolutionChainUrl = new Object(new Object(especie.variavel).evolution_chain).url;
+    let cadeiaEvolutiva = useFetch(evolutionChainUrl);  
+    let cadeia = new Object(new Object(cadeiaEvolutiva.variavel).chain);
+    let nome = [];
+    nome[0] = new Object(cadeia.species).name;
+    nome[1] = new Object(new Object(new Object(cadeia.evolves_to)[0]).species).name;
     
-    //  let {especie, trabalhando} = useFetch(props.species.url);
+    setTimeout(console.log(nome[0], nome[1]), 5000);
+    
+    // let {especie, trabalhando} = useFetch(props.species.url);
     //let {cadeia, trabalhando2} = useFetch(especie.evolution_chain.url);
-    return (<h1>qualquercoisa</h1>
+    return (
+        <>
+            {nome.map((pokemon, key) => {
+                return(
+                    <>
+                        {key > 0 ? "  ->  " : ""}
+                        <Badge>{pokemon}</Badge>
+                    </>
+                )
+            })}
+        </>
     )
 }
 
